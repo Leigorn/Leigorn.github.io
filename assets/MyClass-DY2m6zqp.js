f42b7a6e445edf704171b87d2255f484e1cac97e
Eigo <guoyqiang@foxmail.com>
2024-04-29 00:56:58 +0800

    init

---
+++ assets/MyClass-DY2m6zqp.js
@@ -0,0 +1,1 @@
+import{a3 as s,s as e,v as c}from"./index-B-7oPZT8.js";const a={},t={class:"page"};function o(n,r){return e(),c("div",t," myclass ")}const l=s(a,[["render",o]]);export{l as default};
